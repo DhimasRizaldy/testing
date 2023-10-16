86697f84d135833880f1b17bdc35a7ed91f5a966
imran <muhammadalimran22@gmail.com>
2023-10-17 02:03:16 +0700

    feat: update route and integration for article

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-var express = require('express');
-var logger = require('morgan');
+var express = require("express");
+var logger = require("morgan");
 
-var usersRouter = require('./routes/users.routes');
+var usersRouter = require("./routes/users.routes");
+var usersRouter = require("./routes/article.routes");
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 
-app.use('/api/v1/users', usersRouter);
+app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/article", usersRouter);
 
 module.exports = app;
